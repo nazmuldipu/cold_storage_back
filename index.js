080a97cdcf1b4a1a8a40b0537c630916dbc4779f
Nazmul Alam <nazmuldipu@gmail.com>
2021-03-16 01:09:06 +0600

    port changed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8443;
 const winston = require("winston");
 
 require("./startup/logging")();
