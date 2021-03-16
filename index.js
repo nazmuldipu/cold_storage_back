e036e575adc20969dc81014e9d272e60acc35387
Nazmul Alam <nazmuldipu@gmail.com>
2021-03-16 06:44:22 +0600

    updated

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const winston = require("winston");
 require("./startup/logging")();
 require("./startup/routes")(app);
 require("./startup/db")();
+require("./startup/prod")(app);
+require("./startup/validation")();
 
 app.get("/", (req, res) => {
   res.send({ message: "Weclome to Node REST API" });
