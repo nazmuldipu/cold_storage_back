fc4cb9acb9e657eb8fe3248118f7bc82a20ae874
Nazmul Alam <nazmuldipu@gmail.com>
2021-03-16 07:32:55 +0600

    test 1

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 2096;
-const winston = require("winston");
-
-require("./startup/logging")();
-require("./startup/routes")(app);
-require("./startup/db")();
-// require("./startup/prod")(app);
-require("./startup/validation")();
 
 app.get("/", (req, res) => {
   res.send({ message: "Weclome to Node REST API" });
 });
 
 const server = app.listen(port, () => {
-  winston.info(`Listening on port ${port}...`);
+  console.log(`Listening on port ${port}...`);
 });
 
 module.exports = server;
