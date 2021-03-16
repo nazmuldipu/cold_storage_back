0b584140c51beff7f7441d3a101fe6db34b994db
Nazmul Alam <nazmuldipu@gmail.com>
2021-03-16 06:12:49 +0600

    project setup updated

--- middleware/accesscontrol.js
+++ middleware/accesscontrol.js
@@ -1,24 +1,37 @@
-module.exports = function auth(req, res, next) {
-  // Website you wish to allow to connect accesscontrol
-  res.setHeader("Access-Control-Allow-Origin", "*");
+// module.exports = function auth(req, res, next) {
+//   // Website you wish to allow to connect accesscontrol
+//   res.setHeader("Access-Control-Allow-Origin", "*");
 
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
+//   // Request methods you wish to allow
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+//   );
 
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With, content-type, x-auth-token"
-  );
-  // res.header('Access-Control-Allow-Headers', 'Origin, x-auth-token, Content-Type, Accept, Authorization');
+//   // Request headers you wish to allow
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "X-Requested-With, content-type, x-auth-token"
+//   );
+//   // res.header('Access-Control-Allow-Headers', 'Origin, x-auth-token, Content-Type, Accept, Authorization');
 
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
+//   // Set to true if you need the website to include cookies in the requests sent
+//   // to the API (e.g. in case you use sessions)
+//   res.setHeader("Access-Control-Allow-Credentials", true);
 
-  // Pass to next layer of middleware
-  next();
+//   // Pass to next layer of middleware
+//   next();
+// };
+
+module.exports = function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  // intercept OPTIONS method
+  if ("OPTIONS" == req.method) {
+    res.send(200);
+  } else {
+    next();
+  }
 };
