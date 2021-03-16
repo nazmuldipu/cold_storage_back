e7f24eec774f064990e9eec8b5e1a27d5c85ac28
Nazmul Alam <nazmuldipu@gmail.com>
2021-03-16 08:01:13 +0600

    ssl certificate added

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 2096;
 const winston = require("winston");
+const fs = require('fs');
+const https = require('https');
+
+const options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+};
 
 require("./startup/logging")();
 require("./startup/routes")(app);
@@ -13,7 +20,9 @@ app.get("/", (req, res) => {
   res.send({ message: "Weclome to Node REST API" });
 });
 
-const server = app.listen(port, () => {
+var server = https.createServer(options, app);
+
+server.listen(port, () => {
   winston.info(`Listening on port ${port}...`);
 });
 
